4a1f6fdb0c01db650cfb9cf072a90bab177331c8
udaysarkarud <udaysarkarud@gmail.com>
2021-09-17 00:59:39 +0600

    added js transforming

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const bigPepsi = document.getElementById('big-pepsi')
 const heroArea = document.getElementById('hero-area')
+const logo = document.getElementById('logo')
 
-document.getElementById('pepsi-black').addEventListener('mouseover', () => { changePepsi('pepsi-black', '#000000') })
-document.getElementById('pepsi-blue').addEventListener('mouseover', () => { changePepsi('pepsi-blue', '#E74C3C') })
-document.getElementById('pepsi-gold').addEventListener('mouseover', () => { changePepsi('pepsi-gold', '#F39C12') })
-document.getElementById('pepsi-white').addEventListener('mouseover', () => { changePepsi('pepsi-white', '#5D6D7E') })
+document.getElementById('pepsi-black').addEventListener('mouseover', () => { changePepsi('pepsi-black', '#000000', '360deg') })
+document.getElementById('pepsi-blue').addEventListener('mouseover', () => { changePepsi('pepsi-blue', '#E74C3C', '0deg') })
+document.getElementById('pepsi-gold').addEventListener('mouseover', () => { changePepsi('pepsi-gold', '#F39C12', '360deg') })
+document.getElementById('pepsi-white').addEventListener('mouseover', () => { changePepsi('pepsi-white', '#5D6D7E', '0deg') })
 
-const changePepsi = (pepsiCan, color) => {
+const changePepsi = (pepsiCan, color, rotate) => {
     bigPepsi.src = `images/${pepsiCan}.png`
     heroArea.style.backgroundColor = color
+    logo.style.transform = `rotate(${rotate})`
 }
