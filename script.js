33ff0e99b7f107d0d444f63b7efd9e9350d407bc
udaysarkarud <udaysarkarud@gmail.com>
2021-09-17 00:15:03 +0600

    transition added

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const bigPepsi = document.getElementById('big-pepsi')
 const heroArea = document.getElementById('hero-area')
 
 document.getElementById('pepsi-black').addEventListener('mouseover', () => { changePepsi('pepsi-black', '#000000') })
-document.getElementById('pepsi-blue').addEventListener('mouseover', () => { changePepsi('pepsi-blue', '#7FB3D5') })
+document.getElementById('pepsi-blue').addEventListener('mouseover', () => { changePepsi('pepsi-blue', '#E74C3C') })
 document.getElementById('pepsi-gold').addEventListener('mouseover', () => { changePepsi('pepsi-gold', '#F39C12') })
-document.getElementById('pepsi-white').addEventListener('mouseover', () => { changePepsi('pepsi-white', '#95A5A6') })
+document.getElementById('pepsi-white').addEventListener('mouseover', () => { changePepsi('pepsi-white', '#5D6D7E') })
 
 const changePepsi = (pepsiCan, color) => {
     bigPepsi.src = `images/${pepsiCan}.png`
